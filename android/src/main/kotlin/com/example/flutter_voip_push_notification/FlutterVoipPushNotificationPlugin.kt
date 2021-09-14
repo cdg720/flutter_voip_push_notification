1815be507be15e73b0cf7151633a969b03665102
Dokook Choe <cdg720@gmail.com>
2021-09-14 14:00:51 -0700

    android 2.0 embedding

--- android/src/main/kotlin/com/example/flutter_voip_push_notification/FlutterVoipPushNotificationPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_voip_push_notification/FlutterVoipPushNotificationPlugin.kt
@@ -1,5 +1,7 @@
 package com.example.flutter_voip_push_notification
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -13,6 +15,11 @@ class FlutterVoipPushNotificationPlugin: MethodCallHandler {
       val channel = MethodChannel(registrar.messenger(), "flutter_voip_push_notification")
       channel.setMethodCallHandler(FlutterVoipPushNotificationPlugin())
     }
+  }
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutter_voip_push_notification")
+    channel.setMethodCallHandler(FlutterVoipPushNotificationPlugin());
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
